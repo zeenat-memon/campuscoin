370d7c36102b76b06c319fa5fd8b7603c1ee20e1
zeenat-memon <zntmemon015@gmail.com>
2026-09-25 03:07:40 -0700

    Create Footer.jsx

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,37 @@
+function Footer() {
+  return (
+    <footer className="footer">
+
+      <div className="footer-container">
+
+        <div>
+          <h2>Campus<span>Coin</span></h2>
+          <p>
+            Smart budgeting for smarter student life.
+          </p>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <p>Home</p>
+          <p>About</p>
+          <p>Contact</p>
+        </div>
+
+        <div>
+          <h4>Support</h4>
+          <p>Help Center</p>
+          <p>Privacy Policy</p>
+        </div>
+
+      </div>
+
+      <div className="footer-bottom">
+        © 2026 Campus Coin. All Rights Reserved.
+      </div>
+
+    </footer>
+  );
+}
+
+export default Footer;
