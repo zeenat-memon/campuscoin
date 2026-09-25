ab38a5ff49d4aad478d20f69abb68ffd7375e630
zeenat-memon <zntmemon015@gmail.com>
2026-09-25 12:39:21 -0700

    Update useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,6 +1,6 @@
-import { useAuthContext } from "./context/AuthContext";
+import { useAuthContext } from "../context/AuthContext";
 
-export function useAuth() {
+function useAuth() {
   return useAuthContext();
 }
 
